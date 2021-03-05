b3666a2f5074dd2df2a7e80e02b088fb8eef93dd
Arturo Hernandez <arturo.hernandez5@gmail.com>
2021-03-05 17:56:12 -0600

    implemented a bubblesort function to sort directory file by phone number

--- Main.kt
+++ Main.kt
@@ -11,25 +11,44 @@ fun main() {
     val directory = directoryFile.readLines()
     val searchSize = people.size
 
+    val dirSmall = directory.slice(0..50000)
     val startTime = System.currentTimeMillis()
-    var foundCount = 0
 
-    println("Start searching...")
-
-    for (person in people) {
-        for (listing in directory) {
-            val personName = listing.split(" ").drop(1).joinToString(" ")
-            if (personName == person) {
-                foundCount++
-                break
-            }
-        }
-    }
-
+    //println(small)
+    val smallSorted = bubbleSort(dirSmall)
+    println(smallSorted)
     val endTime = System.currentTimeMillis()
     val duration = endTime - startTime
+    println(printDuration2(duration))
 
-    printDuration(duration, searchSize, foundCount)
+
+
+//    var foundCount = 0
+//
+//    println("Start searching...")
+
+//    for (person in people) {
+//        for (listing in directory) {
+//            val personName = listing.split(" ").drop(1).joinToString(" ")
+//            if (personName == person) {
+//                foundCount++
+//                break
+//            }
+//        }
+//    }
+//
+//    val endTime = System.currentTimeMillis()
+//    val duration = endTime - startTime
+//
+//    printDuration(duration, searchSize, foundCount)
+}
+
+fun printDuration2(msDuration: Long) {
+    val minutes = msDuration / 60_000
+    val seconds = (msDuration - (minutes * 60_000)) / 1000
+    val milliseconds = (msDuration - (minutes * 60_000) - (seconds * 1000))
+
+    println("Time taken: $minutes min. $seconds sec. $milliseconds ms.")
 }
 
 /* Long, Int, Int ->
@@ -42,4 +61,30 @@ fun printDuration(msDuration: Long, searchSize: Int, foundCount: Int) {
     val milliseconds = (msDuration - (minutes * 60_000) - (seconds * 1000))
 
     println("Found $foundCount / $searchSize entries. Time taken: $minutes min. $seconds sec. $milliseconds ms.")
+}
+
+fun bubbleSort(dir: List<String>): MutableList<String> {
+    val directory = dir.toMutableList()
+    var endIndex = directory.size - 1
+    var counter = 1
+
+    while (endIndex > 0) {
+        var swaps = 0
+        for (i in 0 until endIndex) {
+            val firstEntry = directory[i]
+            val firstPerson = directory[i].split(" ")[0]
+            val secondPerson = directory[i + 1].split(" ")[0]
+            if (firstPerson > secondPerson) {
+                directory[i] = directory[i+1]
+                directory[i + 1] = firstEntry
+                swaps++
+            }
+        }
+        //println("Iteration $counter: $people")
+        counter++
+        endIndex--
+        if (swaps == 0) break
+    }
+
+    return directory
 }
